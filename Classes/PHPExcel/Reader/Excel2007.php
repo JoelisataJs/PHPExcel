cac6621af1a84c5247965fb2d8e2c4906d000948
s6igma <jjoelisata@gmail.com>
2018-07-17 22:13:28 +0300

    Fixing bug for reading file with mime type application/zip
    Conflicts:
    	Classes/PHPExcel/Reader/Excel2007.php

--- Classes/PHPExcel/Reader/Excel2007.php
+++ Classes/PHPExcel/Reader/Excel2007.php
@@ -551,35 +551,36 @@ class PHPExcel_Reader_Excel2007 extends PHPExcel_Reader_Abstract implements PHPE
 							self::_readStyle($objStyle, $style);
 							$excel->addCellXf($objStyle);
 						}
-
-						foreach ($xmlStyles->cellStyleXfs->xf as $xf) {
-							$numFmt = PHPExcel_Style_NumberFormat::FORMAT_GENERAL;
-							if ($numFmts && $xf["numFmtId"]) {
-								$tmpNumFmt = self::array_item($numFmts->xpath("sml:numFmt[@numFmtId=$xf[numFmtId]]"));
-								if (isset($tmpNumFmt["formatCode"])) {
-									$numFmt = (string) $tmpNumFmt["formatCode"];
-								} else if ((int)$xf["numFmtId"] < 165) {
-									$numFmt = PHPExcel_Style_NumberFormat::builtInFormatCode((int)$xf["numFmtId"]);
-								}
-							}
-
-							$cellStyle = (object) array(
-								"numFmt" => $numFmt,
-								"font" => $xmlStyles->fonts->font[intval($xf["fontId"])],
-								"fill" => $xmlStyles->fills->fill[intval($xf["fillId"])],
-								"border" => $xmlStyles->borders->border[intval($xf["borderId"])],
-								"alignment" => $xf->alignment,
-								"protection" => $xf->protection,
-								"quotePrefix" => $quotePrefix,
-							);
-							$cellStyles[] = $cellStyle;
-
-							// add style to cellStyleXf collection
-							$objStyle = new PHPExcel_Style;
-							self::_readStyle($objStyle, $cellStyle);
-							$excel->addCellStyleXf($objStyle);
-						}
-					}
+                            
+                            // Fix for file with mime type application/zip
+                        if (! empty ( $xmlStyles->cellStyleXfs )) {
+                            foreach ( $xmlStyles->cellStyleXfs->xf as $xf ) {
+                                $numFmt = PHPExcel_Style_NumberFormat::FORMAT_GENERAL;
+                                if ($numFmts && $xf ["numFmtId"]) {
+                                    $tmpNumFmt = self::getArrayItem ( $numFmts->xpath ( "sml:numFmt[@numFmtId=$xf[numFmtId]]" ) );
+                                    if (isset ( $tmpNumFmt ["formatCode"] )) {
+                                        $numFmt = ( string ) $tmpNumFmt ["formatCode"];
+                                    } elseif (( int ) $xf ["numFmtId"] < 165) {
+                                        $numFmt = PHPExcel_Style_NumberFormat::builtInFormatCode ( ( int ) $xf ["numFmtId"] );
+                                    }
+                                }
+                                $cellStyle = ( object ) array (
+                                        "numFmt" => $numFmt,
+                                        "font" => $xmlStyles->fonts->font [intval ( $xf ["fontId"] )],
+                                        "fill" => $xmlStyles->fills->fill [intval ( $xf ["fillId"] )],
+                                        "border" => $xmlStyles->borders->border [intval ( $xf ["borderId"] )],
+                                        "alignment" => $xf->alignment,
+                                        "protection" => $xf->protection,
+                                        "quotePrefix" => $quotePrefix 
+                                );
+                                $cellStyles [] = $cellStyle;
+                                // add style to cellStyleXf collection
+                                $objStyle = new PHPExcel_Style ();
+                                self::readStyle ( $objStyle, $cellStyle );
+                                $excel->addCellStyleXf ( $objStyle );
+                            }
+                        }
+                    }
 
 					$dxfs = array();
 					if (!$this->_readDataOnly && $xmlStyles) {
